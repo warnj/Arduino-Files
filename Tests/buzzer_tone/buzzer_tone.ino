2fe70a41e6fce044e78cf475f611870eddb9828c
Justin Warner <jkwdives@hotmail.com>
2016-02-04 07:42:33 -0800

    tone add

---
+++ Tests/buzzer_tone/buzzer_tone.ino
@@ -0,0 +1,16 @@
+int tonevalue = 40;
+
+
+void setup() {
+  pinMode(11, OUTPUT);
+}
+
+
+void loop() {
+  tonevalue++;
+  if(tonevalue >= 12000) {
+    tonevalue = 40;
+  }
+tone(11, tonevalue);
+delay(3);
+}
